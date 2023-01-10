e7d820958b80b5f90e01c0d800ce870cb436e710
JansenRJe <jansenrje@outlook.com>
2023-01-10 17:38:39 -0300

    Cadastro de Produtos

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,10 +1,56 @@
-
+import { useState, ChangeEvent } from "react";
 import Head from "next/head";
 import styles from './styles.module.scss'
 import { Header } from "../../components/Header";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 
-export default function Product(){
+import { FiUpload } from 'react-icons/fi'
+
+import { api } from "../../services/apiClient";
+import { setupAPIClient } from "../../services/api";
+
+type ItemProps = {
+    id:string;
+    name: string;
+}
+
+interface CategoryProps{
+    categoryList: ItemProps[];
+}
+
+export default function Product({ categoryList }: CategoryProps){
+
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const [imageAvatar, setImageAvatar] = useState(null);
+
+    const [categories, setCategories] = useState(categoryList || [])
+    const [categorySelected, setCategorySelect] = useState(0)
+
+    function handleFile(e: ChangeEvent<HTMLInputElement>){
+
+        if(!e.target.files){
+            return;
+        }
+
+        const image = e.target.files[0];
+
+        if(!image){
+            return;
+        }
+
+        if(image.type  === 'image/jpeg' || image.type === 'image/png'){
+
+            setImageAvatar(image);
+            setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+
+        }
+    }
+
+    function handleChangeCategory(event){
+        //console.log('Categoria selecionada ', categories[event.target.value])
+        setCategorySelect(event.target.value)
+    }
+
     return(
         <>
             <Head>
@@ -19,32 +65,55 @@ export default function Product(){
 
                     <form className={styles.form}>
 
-                        <select>
-                            <option>
+                        <label className={styles.labelAvatar}>
+                            <span>
+                                <FiUpload size={25} color="#FFF"/>
+                            </span>
+                            
+                            <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
 
-                            </option>
+                            {avatarUrl &&(
+                                <img 
+                                className={styles.preview}
+                                src={avatarUrl} 
+                                alt="foto do produto" 
+                                width={250} 
+                                height={250}
+                                />
+                            )}
+                        </label>
+
+                        <select value={categorySelected} onChange={handleChangeCategory} >
+                            {categories.map( (item, index) =>{
+                                return (
+                                    <option key={item.id} value={index}>
+                                        {item.name}
+                                    </option>
+                                )
+                            })}
                         </select>
 
                         <input 
-                        type="text" 
-                        placeholder="Digite o nome do produto"
-                        className={styles.input}
+                            type="text" 
+                            placeholder="Digite o nome do produto"
+                            className={styles.input}
                         />
 
                         <input 
-                        type="text" 
-                        placeholder="Preço do produto"
-                        className={styles.input}
+                            type="text" 
+                            placeholder="Preço do produto"
+                            className={styles.input}
                         />
-
                         
                         <input 
-                        type="text" 
-                        placeholder="Descreva o produto"
-                        className={styles.input}
+                            type="text" 
+                            placeholder="Descreva o produto"
+                            className={styles.input}
                         />
 
-                        <button className={styles.buttonAdd} type="submit">
+                        <button 
+                            className={styles.buttonAdd} 
+                            type="submit">
                             Cadastrar
                         </button>
                     </form>
@@ -55,9 +124,15 @@ export default function Product(){
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
-    
+    const apiClient = setupAPIClient(ctx)
+
+    const response = await apiClient.get('/category')
+
+    //console.log(response.data);
 
     return {
-        props: {}
+        props: {
+            categoryList: response.data
+        }
     }
 })
